b25e0ec653a2df6fe81c1eb05566caa71f43ee2d
henryhoangduong <henryhoangduong@gmail.com>
2025-06-11 19:21:10 +0700

    remove the celery startup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,6 @@
 # start FastAPI in background
 /app/.venv/bin/fastapi run /app/main.py --port 8080 &
 
-# start Celery in foreground (so Docker tracks it)
-/app/.venv/bin/celery \
-  -A core.celery_config.celery_app worker \
-  --loglevel=info -Q parsing
+# /app/.venv/bin/celery \
+#   -A core.celery_config.celery_app worker \
+#   --loglevel=info -Q parsing
