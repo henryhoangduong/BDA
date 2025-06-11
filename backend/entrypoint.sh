20294d2d7c85d6a5bcec324cce5b3b81f3ca2aeb
henryhoangduong <henryhoangduong@gmail.com>
2025-06-11 20:07:24 +0700

    backend: remove ending & symbol

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # start FastAPI in background
-/app/.venv/bin/fastapi run /app/main.py --port 8080 &
+/app/.venv/bin/fastapi run /app/main.py --port 8080
 
 # /app/.venv/bin/celery \
 #   -A core.celery_config.celery_app worker \
