9ed64cda3e3eeee7195126617907f0a9c0e3b1af
henryhoangduong <henryhoangduong@gmail.com>
2025-06-14 10:14:31 +0700

    backend: run celery

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # start FastAPI in background
-/app/.venv/bin/fastapi run /app/main.py --port 8080
+/app/.venv/bin/fastapi run /app/main.py --port 8080 &
 
-# /app/.venv/bin/celery \
-#   -A core.celery_config.celery_app worker \
-#   --loglevel=info -Q parsing
+/app/.venv/bin/celery \
+  -A core.celery_config.celery_app worker \
+  --loglevel=info -Q parsing
